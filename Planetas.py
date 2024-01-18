fc248c26761bd9d106393e452ef568e1b1e83905
Alvaro <asantant@myuax.com>
2024-01-18 11:31:32 +0100

    Add planetas.py and class Planeta

---
+++ Planetas.py
@@ -0,0 +1,7 @@
+class Planeta:
+    
+    def __init__(self, nombre, volumen, clasificacion):
+        
+        self.nombre = nombre
+        self.volumen = volumen
+        self.clasificacion = clasificacion
