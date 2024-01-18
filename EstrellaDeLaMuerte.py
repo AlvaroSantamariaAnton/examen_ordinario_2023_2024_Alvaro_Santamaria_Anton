c826c7e6c7bcc5964023ae7adde86e9874174228
Alvaro <asantant@myuax.com>
2024-01-18 11:35:22 +0100

    Add EstrellaMuerte.py

---
+++ EstrellaDeLaMuerte.py
@@ -0,0 +1,5 @@
+
+class EstrellaDeLaMuerte:
+    def __init__(self):
+        
+        self.puntos_vida = 1000
--- Planetas.py
+++ Planetas.py
@@ -5,3 +5,17 @@ class Planeta:
         self.nombre = nombre
         self.volumen = volumen
         self.clasificacion = clasificacion
+        
+class Concordia(Planeta):
+    def __init__(self, nombre, volumen, clasificacion):
+        super().__init__(nombre, volumen, clasificacion)
+        
+
+class Ilum(Planeta):
+    def __init__(self, nombre, volumen, clasificacion):
+        super().__init__(nombre, volumen, clasificacion)
+        
+
+class Kamino(Planeta):
+    def __init__(self, nombre, volumen, clasificacion):
+        super().__init__(nombre, volumen, clasificacion)
